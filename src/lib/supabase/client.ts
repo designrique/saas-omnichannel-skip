108cc3c1c5c360c1bd1cf8dda59edf99222a33e0
Skip <skip@usecurling.com>
2025-10-12 04:45:08 +0000

    feat: add supabase integration

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,10 +1,17 @@
-import { createClient } from '@supabase/supabase-js'
+// AVOID UPDATING THIS FILE DIRECTLY. It is automatically generated.
+import { createClient } from '@supabase/supabase-js';
+import type { Database } from './types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY as string;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL and Anon Key must be defined in .env file')
-}
+// Import the supabase client like this:
+// import { supabase } from "@/lib/supabase/client";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+  auth: {
+    storage: localStorage,
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+});
